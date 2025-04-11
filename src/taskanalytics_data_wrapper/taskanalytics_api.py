8bd6e3b1a7c0eb63742dabdc8e786a2b11044f38
Tobias McVey <tobias.mcvey@gmail.com>
2025-04-11 14:34:18 +0200

    added method to download organization metadata and list of surveys

--- src/taskanalytics_data_wrapper/taskanalytics_api.py
+++ src/taskanalytics_data_wrapper/taskanalytics_api.py
@@ -188,3 +188,56 @@ def download_discovery_survey(
         raise SystemExit(err)
     logging.info("Downloaded survey %s", survey_id)
     return data
+
+
+# %%
+def get_organization_metadata(
+    username: str, password: str, organization_id: str, filename_path: str
+) -> requests.Response:
+    """
+    Download metadata for the organization account, including the surveys object
+    """
+    # get
+    # https://api-admin.taskanalytics.com/api/v3/organisations/12559963
+    session = requests.Session()
+    login_url = "https://api-admin.taskanalytics.com/api/v3/auth/login"
+    auth = json.dumps(
+        {"grant_type": "password", "username": username, "password": password}
+    )
+    content_type = "application/json"
+    user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/75.0.3770.90 Chrome/75.0.3770.90 Safari/537.36"
+    headers: dict[str, str | bytes] = {
+        "Content-Type": content_type,
+        "user-agent": user_agent,
+        "Accept": "text/csv",
+    }
+    session.headers = headers
+    try:
+        response = session.post(login_url, data=auth)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
+    obj = json.loads(response.text)
+    try:
+        data = session.get(
+            f"https://api-admin.taskanalytics.com/api/v3/organisations/{organization_id}",
+            timeout=120,
+            headers={
+                "Authorization": "JWT " + obj["access_token"],
+                "Accept": "application/json",
+            },
+        )
+        response.raise_for_status()
+        with tqdm.wrapattr(
+            open(filename_path, "wb"),
+            "write",
+            miniters=1,
+            total=int(data.headers.get("content-length", 0)),
+            desc=filename_path,
+        ) as fout:
+            for chunk in data.iter_content(chunk_size=8192):
+                fout.write(chunk)
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
+    logging.info("Downloaded metadata for organization ID %s", organization_id)
+    return data
